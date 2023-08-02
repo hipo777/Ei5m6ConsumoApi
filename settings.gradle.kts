8b04fe4c8ee15d8fd71091db84539751a13c8359
hipolito <huaylla7@hotmail.com>
2023-08-01 21:22:21 -0400

    Creación del proyecto

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ei5m6 Consumo Api"
+include(":app")
+ 
